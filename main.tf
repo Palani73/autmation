758ec9cfe87c09dad30b233a735d7f94e6f41438
Palani73 <78285994+Palani73@users.noreply.github.com>
2023-11-30 17:06:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "sample" {
   ami                     = "ami-0287a05f0ef0e9d9a"
-  instance-type           = "t2.micro"
+  instance_type           = "t2.micro"
 
    tag {
       Name  = "sample"
